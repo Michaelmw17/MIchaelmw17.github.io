f448bee5980f01e84eb55d12e538ad5d1aed6ee0
Michaelmw17 <Michaelmw17@outlook.com>
2020-10-10 20:56:21 +1100

    removed contact form,conact info added, typos - full-satck

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 //Dark/light
-
-
 function addDarkmodeWidget() {
     
   new Darkmode().showWidget();
@@ -11,7 +9,7 @@ window.addEventListener('load', addDarkmodeWidget);
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+window.onscroll = ()  => {
 scrollFunction();
 };
 
@@ -29,21 +27,21 @@ document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
 };
 
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
+// (function() {
+//       'use strict';
+//       window.addEventListener('load', function() {
+//         // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//         var forms = document.getElementsByClassName('needs-validation');
+//         // Loop over them and prevent submission
+//         var validation = Array.prototype.filter.call(forms, function(form) {
+//           form.addEventListener('submit', function(event) {
+//             if (form.checkValidity() === false) {
+//               event.preventDefault();
+//               event.stopPropagation();
+//             }
+//             form.classList.add('was-validated');
+//           }, false);
+//         });
+//       }, false);
+//     })();
 

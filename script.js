562559c503677017cd1cecc98057647abf7461fe
Michaelmw17 <Michaelmw17@outlook.com>
2020-12-07 23:26:54 +1100

    Clock added to carousel, Fonts added, code clean up and dark mode new

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 //Dark/light
-const addDarkmodeWidget = () => {
-  new Darkmode().showWidget();
+// function addDarkmodeWidget() {
+//     new Darkmode().showWidget();
+//   }
+//   window.addEventListener('load', addDarkmodeWidget);
+const options = {
+  bottom: '10px', // default: '32px'
+  right: '10px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
 }
-window.addEventListener('load', addDarkmodeWidget);
 
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
 //Get the button
 let myButton = document.getElementById("myBtn");
 
@@ -49,4 +64,4 @@ const clock = () => {
       document.getElementById("clock").innerText = time;
     }
     
-    clock();
+clock();
